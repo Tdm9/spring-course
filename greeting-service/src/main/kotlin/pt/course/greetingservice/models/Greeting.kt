9cb5a9b93e3866af810f4ec17769f1002b559430
5XXF2Z2\TiagoMatias <matiastiago99@gmail.com>
2021-10-04 13:20:39 +0100

    adding config files

--- greeting-service/src/main/kotlin/pt/course/greetingservice/models/Greeting.kt
+++
@@ -1,5 +0,0 @@
-package pt.course.greetingservice.models
-
-class Greeting(var id: Long, var description: String) {
-
-}
--- greeting-service/src/main/kotlin/pt/course/greetingservice/models/Person.kt
+++
@@ -1,6 +0,0 @@
-package pt.course.greetingservice.models
-
-import java.io.Serializable
-
-class Person(var firstName: String, var secondNAme: String, var id: Long) : Serializable {
-}
